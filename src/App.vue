05df1d0c56247f9400c6fb6eb4ac8aabc3801bff
Samson Okaludo <samson@divercity.io>
2024-09-05 19:03:03 +0100

    feat: integarte components into app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
+<!-- App.vue or another parent component -->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- Navigation or header section -->
+    <header class="flex justify-between p-4 text-white bg-gray-900">
+      <h1 class="text-xl">My NFT DApp</h1>
+      <ConnectWallet />
+    </header>
+    <main>
+      <div class="flex w-full h-auto flex-rol">
+        <div class="w-1/2 h-auto p-6 border border-gray-300">
+          <MintNFT />
+        </div>
+        <div class="w-1/2 h-auto p-6 border border-gray-300">
+          <TransferAssets />
+        </div>
+      </div>
+      <Gallery />
+    </main>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ConnectWallet from "@/components/ConnectWallet.vue";
+import Gallery from "@/components/Gallery.vue";
+import MintNFT from "@/components/Mint.vue";
+import TransferAssets from "@/components/Transfer.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    ConnectWallet,
+    Gallery,
+    MintNFT,
+    TransferAssets,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* Add any styling as needed */
 </style>
