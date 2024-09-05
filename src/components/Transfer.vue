d55952caf74ef6c53ac7c4c63ef84e5470c23c62
Samson Okaludo <samson@divercity.io>
2024-09-05 19:01:47 +0100

    feat: add asset transfer

---
+++ src/components/Transfer.vue
@@ -0,0 +1,69 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div class="flex flex-col items-center">
+    <h1 class="mb-4 text-2xl">Transfer NFT</h1>
+    <div class="flex flex-row space-x-6">
+      <input
+        type="text"
+        v-model="toAddress"
+        placeholder="Recipient Address"
+        class="px-4 py-2 mb-4 border rounded-lg"
+      />
+      <input
+        type="text"
+        v-model="tokenId"
+        placeholder="Token ID"
+        class="px-4 py-2 mb-4 border rounded-lg"
+      />
+    </div>
+    <button
+      @click="transferNFT"
+      class="px-6 py-3 text-white bg-red-500 rounded-lg"
+    >
+      Transfer NFT
+    </button>
+  </div>
+</template>
+
+<script>
+import { ethers } from "ethers";
+import contractABI from "@/../ContractABI.json";
+
+const contractAddress = process.env.VITE_CONTRACT_ADDRESS;
+
+export default {
+  data() {
+    return {
+      toAddress: "",
+      tokenId: "",
+    };
+  },
+  methods: {
+    async transferNFT() {
+      if (!this.toAddress || !this.tokenId)
+        return alert("Please provide a valid address and token ID");
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        signer
+      );
+
+      try {
+        const tx = await contract.transferFrom(
+          signer.getAddress(),
+          this.toAddress,
+          this.tokenId
+        );
+        await tx.wait();
+        alert("NFT Transferred Successfully!");
+      } catch (err) {
+        console.error(err);
+        alert("Error transferring NFT");
+      }
+    },
+  },
+};
+</script>
